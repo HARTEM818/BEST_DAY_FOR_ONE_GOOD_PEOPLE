4b7ec5a05c96338d7297c82fea432863ecf214ed
HARTEM818 <artemhavryliuk777@gmail.com>
2026-03-02 21:00:28 +0200

    фінальна версія хедеру можливі поправки наприклад анімаціїї і тд

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,16 @@
 import "./App.css";
 import Header from "../header/Header";
 
-// import { Routes, Route } from "react-router";
 
 export default function App() {
   return (
     <>
-      {/* <Routes>
-        <Route path="./game" />
-      </Routes> */}
+    
       <Header />
-      <main></main>
+      
+      <main>
+
+      </main>
     </>
   );
 }
--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,28 +1,40 @@
 import Container from "../hoc/Container";
 import giftIcon from "../img/lucide-Gift-Outlined.svg";
-import { Link } from "react-router";
-import "./Header.css"
+import giftMenu from "../img/lucide-Menu-Outlined.svg";
+import "./Header.css";
+import { useState } from "react";
+
 export default function Header() {
+  const [isMenu, setMenu] = useState(false);
+
   return (
-    <>
-      <header>
-        <Container className="header__wrapper-title">
-          <Container className="header__wrapper-img">
-            <img src={giftIcon} alt="" />
-          </Container>
-          <p className="header__title">HR Birthday</p>
+    <header className="header">
+      <Container className="header__wrapper-title">
+        <Container className="header__wrapper-img">
+          <img src={giftIcon} alt="" />
         </Container>
-        <nav>
-          <ul>
-            <li>
-              <Link to="./game">Квести</Link>
-            </li>
-            <li>
-              <Link to="./wish">Побажання від рідних</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </>
+        <p className="header__title">HR Birthday</p>
+      </Container>
+
+
+      
+      <img
+        onClick={() => setMenu((prev) => !prev)}
+        src={giftMenu}
+        alt="Меню"
+        className="header__menu-icon"
+      />
+
+      <nav className={`header__nav ${isMenu ? "active" : ""}`}>
+        <ul className="header__list">
+          <li>
+            <a href="#game">Обовязвокі Квести</a>
+          </li>
+          <li>
+            <a href="#wish">Побажання від рідних</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
