709b5665f062029bce1ae7cd07223adf992ad2f3
HARTEM818 <artemhavryliuk777@gmail.com>
2026-03-06 08:43:47 +0200

    майже закінчена 2 гра треба тільки зробити передачу днаих на хомку і тд

--- src/pages/game2/Game2.tsx
+++ src/pages/game2/Game2.tsx
@@ -1,21 +1,87 @@
 import Container from "../../hoc/Container";
-import "./Game2.css"
+import "./Game2.css";
+import { useState } from "react";
 
 export default function Game2() {
+  const [start, setStart] = useState(false);
+  const [selected, setSelected] = useState<number | null>(null);
+  const [shuffle, setShuffle] = useState(false);
+  const [guessPhase, setGuessPhase] = useState(false);
+  const [flipCorrect, setFlipCorrect] = useState(false); // новий стан
+
+  const handleClick = (index: number) => {
+    
+    if (selected === null) {
+      setSelected(index);
+      setShuffle(true);
+
+      setTimeout(() => {
+        setGuessPhase(true);
+      }, 2000);
+
+      return;
+    }
+
+  
+    if (guessPhase) {
+      if (index === selected) {
+        setFlipCorrect(true); 
+        alert("Правильно 🎉");
+      } else {
+        location.reload();
+        alert("Неправильно 😅");
+      }
+    }
+  };
+
   return (
-    <>
-      <Container className="game2__container">
-        <Container className="game2-wrapper__title">
-            <h2 className="game2__title">Це вже друга гра перед тим як ти отримаєш передбачення</h2>
-            <p className="game2__text-description">Суть цієї гри відслідкувати за вибраним тобою блоком і клікнути по ньому тоді коли всі блоки перемішаються</p>
-        </Container>
-        <button className="game2__start ">почати</button>
-        <Container className="game2-wrapper__btn"> 
-            <button className="game2__btn">Добро</button>
-            <button className="game2__btn">Магія</button>
-            <button className="game2__btn">Свято</button>
-        </Container>
+    <Container className="game2__container">
+      <Container className="game2-wrapper__title">
+        <h2 className="game2__title">
+          Це вже друга гра перед тим як ти отримаєш передбачення
+        </h2>
+
+        <p className="game2__text-description">
+          Суть гри — відслідкувати вибрану кнопку
+        </p>
       </Container>
-    </>
+
+      <button onClick={() => setStart(true)} className="game2__start">
+        почати
+      </button>
+
+      {start && selected === null && <p className="game2__start-text">Вибери кнопку</p>}
+      {start && selected !== null && !guessPhase && <p className="game2__start-text">Слідкуй за кнопкою</p>}
+      {guessPhase && <p className="game2__start-text">Тепер знайди свою кнопку</p>}
+
+      <Container className="game2-wrapper__btn">
+        <button
+          onClick={() => start && handleClick(0)}
+          className={`game2__btn
+            ${shuffle && start ? "animation-first" : ""}
+            ${flipCorrect ? "flip-up" : ""}`}
+        >
+          Добро
+        </button>
+
+        <button
+          onClick={() => start && handleClick(1)}
+          className={`game2__btn
+            ${shuffle && start ? "animation-second" : ""}
+            ${flipCorrect ? "flip-up" : ""}`}
+        >
+          Магія
+        </button>
+
+        <button
+          onClick={() => start && handleClick(2)}
+          className={`game2__btn
+            ${shuffle && start ? "animation-third" : ""}
+            ${flipCorrect ? "flip-up" : ""}`}
+        >
+          Свято
+        </button>
+      </Container>
+    </Container>
   );
 }
