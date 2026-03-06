9386c574c65a49ecbd8ea6efb9383777c42e59d1
HARTEM818 <artemhavryliuk777@gmail.com>
2026-03-06 18:31:19 +0200

    билд

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./app/App.tsx";
-import { HashRouter } from "react-router";
-createRoot(document.getElementById("root")!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
-);
